79a2f73b47d4579902ec57009a924215803bd39d
Ayush Jha <jhaa97204@gmail.com>
2026-03-19 10:59:34 +0530

    temp rename

--- FrontEnd/src/app.routes.jsx
+++ FrontEnd/src/app.routes.jsx
@@ -2,8 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from './auth/pages/Login'
 import Register from './auth/pages/Register'
 import Protected from "./auth/components/Protected";
-import Home from "./interview/pages/Home";
-import Interview from "./interview/pages/Interview";
+import Home from "./interview/pages/Home.jsx";
+import Interview from "./interview/pages/Interview.jsx";
 
 export const router = createBrowserRouter([
     {
